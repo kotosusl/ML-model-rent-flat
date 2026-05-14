f5c82d5c82358e18c9a2d19d696d1d510d578d9e
Elizaveta Lavretskaia <108192472+kotosusl@users.noreply.github.com>
2026-05-14 12:36:17 +0300

    change test_parser.py directory

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,8 +1,10 @@
 import csv
+import os
 
 
 def test_csv_file():
-    filename = "../csv_files/main_data10000.csv"
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(test_dir, '..', 'csv_files', 'main_data10000.csv')
 
     with open(filename, 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
